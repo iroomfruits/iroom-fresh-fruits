91b12f6f37e3791989765f33dd4b50dc443d29e1
iroomfruits <polo4562@gmail.com>
2026-09-11 01:53:19 +0900

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE='iroom-v31-myiroom-label-clean';
-const ASSETS=['./','./index.html','./iroom_assets/iroom-v30.css','./iroom_assets/iroom-v30.js','./iroom_assets/iroom-logo-final.png','./manifest.webmanifest'];
+const CACHE='iroom-v32-soft-scenery-final';
+const ASSETS=['./','./index.html','./iroom_assets/iroom-v32.css','./iroom_assets/iroom-v32.js','./iroom_assets/iroom-logo-final.png','./iroom_assets/hero_faded/spring.jpg','./iroom_assets/hero_faded/summer.jpg','./iroom_assets/hero_faded/autumn.jpg','./iroom_assets/hero_faded/winter.jpg','./manifest.webmanifest'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});return r}).catch(()=>caches.match(e.request)))})
