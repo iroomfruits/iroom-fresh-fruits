0f56c44065bb8968d4a5c1ec7c8dfcba1d0e297c
iroomfruits <polo4562@gmail.com>
2026-09-10 22:22:04 +0900

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='iroom-v30-reference-exact';
+const CACHE='iroom-v31-myiroom-label-clean';
 const ASSETS=['./','./index.html','./iroom_assets/iroom-v30.css','./iroom_assets/iroom-v30.js','./iroom_assets/iroom-logo-final.png','./manifest.webmanifest'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
