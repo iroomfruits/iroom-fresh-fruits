4174269c838458812358c62215b8dd929eca6841
iroomfruits <polo4562@gmail.com>
2026-09-17 00:12:29 +0900

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
-const CACHE='iroom-v60-11-unified-cta';
+const CACHE='iroom-v60-12-unified-cta-css-fix';
 const CORE=[
   './','./index.html','./manifest.webmanifest','./favicon.ico',
-  './iroom_assets/iroom-v60-11.css','./iroom_assets/iroom-v60-11.js','./iroom_assets/iroom-logo-final.png',
+  './iroom_assets/iroom-v60-12.css','./iroom_assets/iroom-v60-12.js','./iroom_assets/iroom-logo-final.png',
   './iroom_assets/iroom-app-icon-32-v60-10.png','./iroom_assets/iroom-app-icon-64-v60-10.png',
   './iroom_assets/iroom-app-icon-180-v60-10.png','./iroom_assets/iroom-app-icon-192-v60-10.png',
   './iroom_assets/iroom-app-icon-512-v60-10.png','./iroom_assets/iroom-app-icon-512-maskable-v60-10.png'
